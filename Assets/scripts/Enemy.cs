f77a5a1e32bb1016552fa5b7417f5ceb80a7028f
PMark-est <markopeedosk@gmail.com>
2024-10-26 14:04:02 +0300

    Enemy prefab made

---
+++ Assets/scripts/Enemy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float Speed = 2f;
+    public float MoveDistance = 3f;
+    public Sprite Sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

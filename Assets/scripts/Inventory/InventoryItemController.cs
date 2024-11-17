df4f37b038852f759b5d57581724e049d637f9d4
arnold <arnold.arney@gmail.com>
2024-11-17 10:05:24 +0200

    pillers

--- Assets/scripts/Inventory/InventoryItemController.cs
+++ Assets/scripts/Inventory/InventoryItemController.cs
@@ -7,39 +7,8 @@ public class InventoryItemController : MonoBehaviour
 {
     Item item;
 
-
-    public void RemoveItem()
-    {
-        InventoryManager.Instance.Remove(item);
-
-        Destroy(gameObject);
-    }
-
     public void AddItem(Item newItem)
     {
         item = newItem;
-
     }
-    public int GetItemId()
-    {
-        if (item == null)
-        {
-            Debug.LogError("Item is null in InventoryItemController!");
-            return -1; // Return a default value or handle it as needed
-        }
-        return item.id; // Assuming item.id is valid
-    }
-
-
-    public void UseItem() 
-    {
-        switch (item.itemType) 
-        {
-            case Item.ItemType.Potion:
-                break;
-            case Item.ItemType.Book:
-                break;
-        }
-        RemoveItem();
-    }   
 }

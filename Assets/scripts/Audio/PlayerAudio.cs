fffd900a3ee5ac7b382e44cf04e0d6c47ac5b0c8
RobinMurk <murkrobin@gmail.com>
2025-01-17 16:38:49 +0200

    added walking sound

---
+++ Assets/scripts/Audio/PlayerAudio.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerAudio : MonoBehaviour
+{
+
+    public AudioManager manager;
+
+    public void StepSound(int whatFoot){
+        int numbr = UnityEngine.Random.Range(1,4);
+        String audioName = "Footstep" + numbr.ToString();
+        manager.Play(audioName);
+    }
+}

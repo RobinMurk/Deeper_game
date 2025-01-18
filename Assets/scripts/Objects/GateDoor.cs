a023a5fb0d188caa9f51f12408daae764a85309a
PMark-est <markopeedosk@gmail.com>
2025-01-18 12:19:44 +0200

    level1 changes

--- Assets/scripts/Objects/GateDoor.cs
+++ Assets/scripts/Objects/GateDoor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -6,41 +7,14 @@ using UnityEngine;
 public class GateDoor : MonoBehaviour
 {
     public static GateDoor Instance;
-    public GameObject Gate;
-    [Range(0,2)]
-    public float GateSpeed;
-    private Vector3 currentPos;
-    private Vector3 openPos;
-    private bool startOpening;
 
-    private void Start() {
+    private void Awake()
+    {
         Instance = this;
-        currentPos = Gate.transform.position;
-        openPos = new Vector3(currentPos.x,currentPos.y - 30 ,currentPos.z);
-
-
-    }
-    private void Update() {
-        if(startOpening){
-            
-            Gate.transform.position = Vector3.MoveTowards(
-            currentPos,
-            openPos,
-            GateSpeed * Time.deltaTime
-        );
-        }
-
-        float distance = Vector3.Distance(currentPos, openPos);
-        if(distance <= 0.01f){
-            startOpening = false;
-        }
     }
 
     public void OpenGate(){
-        startOpening = true;
-        //Gate.GetComponent<BoxCollider2D>().isTrigger = true;
-        //temp
-        Gate.gameObject.SetActive(false);
+        gameObject.GetComponent<Animator>().SetBool("openGate", true);
         FindObjectOfType<AudioManager>().Play("GateOpening");
     }
 }
